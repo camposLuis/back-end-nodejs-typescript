cad0150c140793aa1ad2680e0eb6123da61bfad9
camposLuis <luis.campos.henrique@hotmail.com>
2020-04-19 12:38:21 -0300

    Importing ts files

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
+app.use(routes);
 app.use(express.json());
-
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
